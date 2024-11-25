93639a1099c26a083336da79fffc7902ee981b30
Fredy Samuel B Tarigan <fredy@tarigan.id>
2024-11-25 15:59:26 +0700

    generate status from message

--- api-response/src/http/axum_response.rs
+++ api-response/src/http/axum_response.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use axum::{
     http::{header, HeaderMap, StatusCode},
     response::{IntoResponse, Response},
@@ -12,6 +14,12 @@ impl IntoResponse for ApiHttpResponse {
         let status_code =
             StatusCode::from_u16(self.code).unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
 
+        let status = StatusCode::from_u16(self.code)
+            .unwrap_or_default()
+            .to_string();
+
+        println!("Status: {:?}", status);
+
         let mut headers = HeaderMap::new();
         headers.insert(header::CONTENT_TYPE, "application/json".parse().unwrap());
         headers.insert(header::ACCEPT, "application/json".parse().unwrap());
@@ -20,7 +28,7 @@ impl IntoResponse for ApiHttpResponse {
             "code": self.code,
             "message": self.message,
             "data": self.data,
-            "status": self.status,
+            "status": status,
             "errors": self.errors
         });
 
